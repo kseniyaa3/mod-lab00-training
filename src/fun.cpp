f28cf0b0f97d36c76644713536d0e0489a4a500b
kseniyaa3 <98958076+kseniyaa3@users.noreply.github.com>
2022-03-06 13:27:26 +0300

    changed2

--- src/fun.cpp
+++ src/fun.cpp
@@ -5,10 +5,8 @@ int64_t power(int64_t x, uint16_t n) {
     int64_t num = x;
     int64_t res = 1;
     uint16_t pow = n;
-    for (uint16_t i = 1; i <= pow; i++)
-    {
+    for (uint16_t i = 1; i <= pow; i++) {
         res *= num;
-
     }
     return res;
 }
